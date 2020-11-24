83a2d8d939628b50a6f6f7c5521d6dc50401d9a3
Sabit <kondakcisabit@gmail.com>
2020-11-24 16:38:54 +0300

    Update MyBinaryHeap.cs

--- SortingAlgorithm/MyBinaryHeap.cs
+++ SortingAlgorithm/MyBinaryHeap.cs
@@ -111,7 +111,7 @@ namespace SortingAlgorithms
         {
             T temp = array[0];
             Swap(0, lastPosition); // Kaldirma islemi 0. indeksteki sayi son indekse tasinarak yapilir
-            lastPosition--;    //lastPosition-- azalilarak son pozisyondaki nesneye ait referans bosta kalir ve garbage collector tarafindan silinir.
+            lastPosition--;    //lastPosition-- azalilarak son pozisyondaki nesneye ait referans bosta kalir.
             TrickleDown(0);   // Ebeveyn'den Cocuga dogru 0. indeksteki deger heap kurallarina uyumlu bir sekilde tasinir.
             return temp;
         }
