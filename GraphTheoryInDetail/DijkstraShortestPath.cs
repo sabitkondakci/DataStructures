08f0a85f99785744c1bfe6a85a8ea684c7927b55
Sabit Kondakçı <kondakcisabit@gmail.com>
2020-12-18 23:16:08 +0300

    Update DijkstraShortestPath.cs

--- GraphTheoryInDetail/DijkstraShortestPath.cs
+++ GraphTheoryInDetail/DijkstraShortestPath.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace GraphTheoryInDetail
 {
-    // shortest path for undirected graphs , non negative edge weights
+    // shortest path for directed graphs , non negative edge weights
     // O(E*log(V)) time complexity
     class DijkstraShortestPath
     {
@@ -21,24 +21,29 @@ namespace GraphTheoryInDetail
         public DijkstraShortestPath(int numOfVertices)
         {
             myGraph = new List<KeyValuePair<int, double>>[numOfVertices];
-           
+
             for (int i = 0; i < numOfVertices; i++)
             {
                 myGraph[i] = new List<KeyValuePair<int, double>>();
             }
         }
-        public void CreateUnDirectedEdges(int fromVer, int toVer, double weight)
+        public void CreateDirectedEdges(int fromVer, int toVer, double weight)
         {
             myGraph[fromVer].Add(new KeyValuePair<int, double>(toVer, weight));
-            myGraph[toVer].Add(new KeyValuePair<int, double>(fromVer, weight));
         }
-        public double LazyDijkstraShortPath(List<KeyValuePair<int,double>>[] graphAdjList,
-            int startNode,int endNode,out bool[] path)
+        public double LazyDijkstraShortPath(List<KeyValuePair<int, double>>[] graphAdjList,
+            int startNode, int endNode, out List<int> path)
         {
             int numOfVertices = graphAdjList.Length;
+
+            if (endNode < 0 || endNode >=numOfVertices ) throw new ArgumentOutOfRangeException("Invalid node index");
+            if (startNode < 0 || startNode >= numOfVertices) throw new ArgumentOutOfRangeException("Invalid node index");
+            
+            
             bool[] visited = new bool[numOfVertices];
             double[] distance = new double[numOfVertices];
-            bool[] resultPath=new bool[numOfVertices];//path you visit
+            List<int> resultPath = new List<int>();//path you visit
+            int[] prevNode = new int[numOfVertices];
 
             for (int i = 0; i < numOfVertices; i++)
             {
@@ -47,15 +52,12 @@ namespace GraphTheoryInDetail
 
             distance[startNode] = 0;
 
-            resultPath[startNode] = true;
-            resultPath[endNode] = true;
+            SimplePriorityQueue<int, double> priorityQueue =
+                new SimplePriorityQueue<int, double>();
 
-            SimplePriorityQueue<int,double> priorityQueue=
-                new SimplePriorityQueue<int,double>();
+            priorityQueue.Enqueue(startNode, 0.0);
 
-            priorityQueue.Enqueue(startNode,0.0);
-
-            while (priorityQueue.Count!=0)
+            while (priorityQueue.Count != 0)
             {
                 int indexofNode = priorityQueue.Dequeue();
                 visited[indexofNode] = true;
@@ -66,7 +68,7 @@ namespace GraphTheoryInDetail
 
                     if (distance[keyValuePair.Key] < minValue)
                         continue;
-
+                    
                     if (visited[keyValuePair.Key])
                         continue;
 
@@ -74,31 +76,45 @@ namespace GraphTheoryInDetail
 
                     if (distNew < distance[keyValuePair.Key])
                     {
-                        resultPath[indexofNode] = true;
+                        prevNode[keyValuePair.Key] = indexofNode;
                         distance[keyValuePair.Key] = distNew;
                         priorityQueue.Enqueue(keyValuePair.Key, distNew);
                     }
-                    
                 }
 
                 if (indexofNode == endNode)
                 {
+                    for (int at = endNode; at != 0; at = prevNode[at]) 
+                        resultPath.Add(at);
+
+                    if(!resultPath.Contains(startNode))
+                        resultPath.Add(startNode);
+                    if(!resultPath.Contains(endNode))
+                        resultPath.Add(endNode);
+
+                    resultPath.Reverse();
+
                     path = resultPath;
                     return distance[endNode];
                 }
-                    
+
             }
 
             path = resultPath;
             return double.PositiveInfinity;
         }
         public double EagerDijkstraShortPath(List<KeyValuePair<int, double>>[] graphAdjList,
-            int startNode, int endNode,out bool[] path)
+            int startNode, int endNode, out List<int> path)
         {
             int numOfVertices = graphAdjList.Length;
+
+            if (endNode < 0 || endNode >= numOfVertices) throw new ArgumentOutOfRangeException("Invalid node index");
+            if (startNode < 0 || startNode >= numOfVertices) throw new ArgumentOutOfRangeException("Invalid node index");
+
             bool[] visited = new bool[numOfVertices];
             double[] distance = new double[numOfVertices];
-            bool[] resultPath = new bool[numOfVertices];//path you visit
+            List<int> resultPath = new List<int>();//path you visit
+            int[] prevNode = new int[numOfVertices];
 
             for (int i = 0; i < numOfVertices; i++)
             {
@@ -106,9 +122,6 @@ namespace GraphTheoryInDetail
             }
 
             distance[startNode] = 0;
-
-            resultPath[startNode] = true;
-            resultPath[endNode] = true;
 
             SimplePriorityQueue<int, double> priorityQueue =
                 new SimplePriorityQueue<int, double>();
@@ -134,19 +147,30 @@ namespace GraphTheoryInDetail
 
                     if (distNew < distance[keyValuePair.Key])
                     {
-                        resultPath[indexofNode] = true;
+                        prevNode[keyValuePair.Key] = indexofNode;
                         distance[keyValuePair.Key] = distNew;
-                        if(!priorityQueue.EnqueueWithoutDuplicates(keyValuePair.Key, distNew))
-                            priorityQueue.UpdatePriority(keyValuePair.Key,distNew);
+                        if (!priorityQueue.EnqueueWithoutDuplicates(keyValuePair.Key, distNew))
+                            priorityQueue.UpdatePriority(keyValuePair.Key, distNew);
+                        
                     }
                 }
 
                 if (indexofNode == endNode)
                 {
+                    for (int at = endNode; at != 0; at = prevNode[at])
+                        resultPath.Add(at);
+
+                    if (!resultPath.Contains(startNode))
+                        resultPath.Add(startNode);
+                    if (!resultPath.Contains(endNode))
+                        resultPath.Add(endNode);
+
+                    resultPath.Reverse();
+
                     path = resultPath;
                     return distance[endNode];
                 }
-                    
+
             }
 
             path = resultPath;
