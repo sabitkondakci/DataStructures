dc003ca96719c7bc4f6be340f3a889c357fbae37
Sabit Kondakçı <kondakcisabit@gmail.com>
2020-12-19 00:01:11 +0300

    Update BellmanFordNegativeWeight.cs

--- GraphTheoryInDetail/BellmanFordNegativeWeight.cs
+++ GraphTheoryInDetail/BellmanFordNegativeWeight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -39,19 +39,15 @@ namespace GraphTheoryInDetail
             // Only in the worst case does it take numberOfVertices-1 iterations for the Bellman-Ford
             // Another stopping condition is when we're unable to
             // relax an edge, this means we have reached the optimal solution early.
-            bool relaxedAnEdge = true;
-
+            
             // For each vertex, apply relaxation for all the edges
-            for (int v = 0; v < numberOfVertices - 1 && relaxedAnEdge; v++)
+            for (int v = 0; v < numberOfVertices - 1 ; v++)
             {
-                relaxedAnEdge = false;
-
                 foreach (var edge in myAdjGraph[v])
                 {
                     if (distance[v] + edge.Value < distance[edge.Key])
                     {
                         distance[edge.Key] = distance[v] + edge.Value;
-                        relaxedAnEdge = true;
                     }
                 }
             }
@@ -59,16 +55,15 @@ namespace GraphTheoryInDetail
             // Run algorithm a second time to detect which nodes are part
             // of a negative cycle. A negative cycle has occurred if we
             // can find a better path beyond the optimal solution.
-            relaxedAnEdge = true;
-            for (int v = 0; v < numberOfVertices - 1 && relaxedAnEdge; v++)
+          
+
+            for (int v = 0; v < numberOfVertices - 1 ; v++)
             {
-                relaxedAnEdge = false;
                 foreach (var edge in myAdjGraph[v])
                 {
                     if (distance[v] + edge.Value < distance[edge.Key])
                     {
                         distance[edge.Key] = double.NegativeInfinity;
-                        relaxedAnEdge = true;
                     }
                 }
             }
